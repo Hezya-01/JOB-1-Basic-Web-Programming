9c8682b0f16cf0db7ded4468b3de63fa16d152a0
Hezya-01 <mahezjuniandi@gmail.com>
2025-07-25 01:08:28 +0700

    Perbaikan

--- assets/Login.js
+++ assets/Login.js
@@ -5,11 +5,11 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   const password = document.getElementById('password').value;
   const errorElement = document.getElementById('error-message');
 
-  const validUsername = "admin";
-  const validPassword = "12345";
+  const validUsername = "Cihuy";
+  const validPassword = "123456";
 
   if (username === validUsername && password === validPassword) {
-    window.location.href = "Dashboard.html";
+    window.location.href = "dashboard.html";
   } else {
     errorElement.textContent = "Username atau password salah!";
   }
